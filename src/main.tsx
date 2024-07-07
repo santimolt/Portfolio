2fea70c6cbb03ce373d7775e5a8237600908ae3a
santimol <santimolt@gmail.com>
2024-07-06 21:36:31 -0300

    Fix de archivo no usado bloqueando el publish

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import Header from "./header/Header.tsx";
 import Experience from "./experience/Experience.tsx";
 import Skills from "./skills/Skills.tsx";
 import Navigation from "./navigation/Navigation.tsx";
-import CurriculumFooter from "./contact/CurriculumFooter.tsx";
+// import CurriculumFooter from "./contact/CurriculumFooter.tsx";
 
 import "./main.css";
 
